2c30f6a33db35e91582e72d2c506b432e7426150
Ak <akalankapathirage22@gmail.com>
2022-06-17 18:54:06 +0530

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import AuctionOpen from "./containers/AuctionOpen";
 import abi from "./settings/abi.json";
 import contractAddrs from "./settings/contractAddresses.json";
 import { useMoralis, useWeb3Contract } from "react-moralis";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import AuctionState from "./components/AuctionState";
+import AuctionClosed from "./containers/AuctionClosed";
 
 export interface IContractAddrs {
   [key: string]: string;
@@ -14,10 +15,9 @@ function App() {
   const addrs: IContractAddrs = contractAddrs;
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
   const chainId = chainIdHex ? parseInt(chainIdHex, 16).toString() : null;
-  const [ auctionState, setAuctionState ] = useState(0)
+  const [auctionState, setAuctionState] = useState(0);
 
-  const { runContractFunction : getauctionState  } =
-  useWeb3Contract({
+  const { runContractFunction: getauctionState } = useWeb3Contract({
     abi: abi,
     contractAddress: chainId ? addrs[chainId] : undefined,
     functionName: "s_auctionState",
@@ -25,24 +25,33 @@ function App() {
 
   const fetchData = async () => {
     const auctionState = await getauctionState({
-      onSuccess: (auctionState: any) => console.log(auctionState, "auctionState"),
-      onError: (err: Error) => console.error(err)
+      onSuccess: (auctionState: any) =>
+        console.log(auctionState, "auctionState"),
+      onError: (err: Error) => console.error(err),
     });
     setAuctionState(auctionState as number);
-  }
+  };
 
   useEffect(() => {
     if (isWeb3Enabled) {
-        fetchData()
+      fetchData();
     }
-}, [isWeb3Enabled])
-  
+  }, [isWeb3Enabled]);
+
   return (
     <>
       <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
         <Header />
-        <AuctionState auctionState={auctionState}/>
-        <AuctionOpen addrs={addrs} chainId={chainId} auctionState={auctionState}/>
+        <AuctionState auctionState={auctionState} />
+        {auctionState === 0 ? (
+          <AuctionClosed />
+        ) : (
+          <AuctionOpen
+            addrs={addrs}
+            chainId={chainId}
+            auctionState={auctionState}
+          />
+        )}
       </div>
     </>
   );
--- src/containers/AuctionOpen.tsx
+++ src/containers/AuctionOpen.tsx
@@ -1,7 +1,7 @@
 import EnterAuction from "../components/auctionActions/EnterAuction";
 import PlaceBid from "../components/auctionActions/PlaceBid";
 import ProductCard from "../components/ProductCard";
-import { IContractAddrs } from "../App"
+import { IContractAddrs } from "../App";
 
 export interface IAuctionProps {
   addrs: IContractAddrs;
@@ -9,16 +9,22 @@ export interface IAuctionProps {
   auctionState: number;
 }
 
-const AuctionOpen = ({addrs, chainId, auctionState}: IAuctionProps) => {
+const AuctionOpen = ({ addrs, chainId, auctionState }: IAuctionProps) => {
   return (
     <div className="max-w-sm mx-auto space-y-5">
       <ProductCard />
 
       <div className="flex rounded-md shadow-sm justify-center" role="group">
-        <EnterAuction addrs={addrs} chainId={chainId} auctionState={auctionState}/>
-        {/* <PlaceBid /> */}
+        {auctionState === 1 ? (
+          <EnterAuction
+            addrs={addrs}
+            chainId={chainId}
+            auctionState={auctionState}
+          />
+        ) : (
+          <PlaceBid />
+        )}
       </div>
-
     </div>
   );
 };
