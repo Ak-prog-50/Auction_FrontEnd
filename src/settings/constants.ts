f109b47aab455635d1ea7897c1024ea640fd43cb
Ak <akalankapathirage22@gmail.com>
2022-06-18 16:44:14 +0530

    constants

--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -1,1 +1,3 @@
 export const ETHERSCAN_URL = "https://etherscan.io/";
+export const NFT_ADDR = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const ERC20_ADDR = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
