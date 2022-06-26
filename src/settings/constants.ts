f6c0b83859dc863ed5c67335c63108e77c8eefc8
Ak <akalankapathirage22@gmail.com>
2022-06-26 12:33:36 +0530

    new deployment

--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -1,6 +1,6 @@
 import { parseEther } from "@ethersproject/units";
 
-export const ETHERSCAN_URL = "https://rinkeby.etherscan.io/address/0xf5fe7ee350847f5b656e2cc0dd9f75576596803b#readContract";
+export const ETHERSCAN_URL = "https://rinkeby.etherscan.io/address/0x3d08a50da1f2764222bafdda4d51b65cd3f46ab2#readContract";
 export const NFT_ADDR = "0x7272924794377Dd0EF0E92c0F675beb488b32443";
 export const ERC20_ADDR = "0x35f27f917C3F73DBD1015A39d0C63FecDBf43d6D";
 export const MAX_TOKENS = 100;
