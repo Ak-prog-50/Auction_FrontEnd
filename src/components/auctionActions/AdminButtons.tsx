f4761b8af88677885adbaa5f9369ae3e5fdbfd05
Ak <akalankapathirage22@gmail.com>
2022-06-26 12:00:29 +0530

    withdraw button + redirect to etherscan

--- src/components/auctionActions/AdminButtons.tsx
+++ src/components/auctionActions/AdminButtons.tsx
@@ -9,6 +9,7 @@ import {
 import useAuctionCalls from "../../hooks/useAuctionCalls";
 import Spinner from "../Spinner";
 import { initSetup } from "../../helperFunctions/contractQueries";
+import { ETHERSCAN_URL } from "../../settings/constants";
 
 interface IAdminButtonsProps {
   isOwner: boolean
@@ -112,6 +113,14 @@ const AdminButtons = ({ isOwner }: IAdminButtonsProps) => {
         ) : (
           <span>End Auction</span>
         )}
+      </button>
+
+      <button
+        type="button"
+        className="block w-1/3 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"
+        onClick={() => window.location.href = ETHERSCAN_URL}
+      >
+        <span>Withdraw Funds</span>
       </button>
     </div>
   );
