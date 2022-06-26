13cee264a66681993c07a05dbdd103605148dc74
Ak <akalankapathirage22@gmail.com>
2022-06-26 11:23:01 +0530

    abi + addrs

--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -1,7 +1,7 @@
 import { parseEther } from "@ethersproject/units";
 
-export const ETHERSCAN_URL = "https://etherscan.io/";
-export const NFT_ADDR = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const ERC20_ADDR = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const ETHERSCAN_URL = "https://rinkeby.etherscan.io/address/0xf5fe7ee350847f5b656e2cc0dd9f75576596803b#readContract";
+export const NFT_ADDR = "0x7272924794377Dd0EF0E92c0F675beb488b32443";
+export const ERC20_ADDR = "0x35f27f917C3F73DBD1015A39d0C63FecDBf43d6D";
 export const MAX_TOKENS = 100;
 export const NFT_TOKEN_ID = parseEther("0") // parseEther is used as a replacement to BigNumber.from(0)
