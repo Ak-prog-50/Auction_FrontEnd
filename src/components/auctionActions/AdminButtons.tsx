0c2081468748c4d2a0b385f1e0c4b3aec2c53d9a
Akalanka <81894220+Ak-prog-50@users.noreply.github.com>
2022-10-16 20:47:24 +0530

    withdraw funds blockchain url

--- src/components/auctionActions/AdminButtons.tsx
+++ src/components/auctionActions/AdminButtons.tsx
@@ -127,7 +127,7 @@ const AdminButtons = ({ isOwner }: IAdminButtonsProps) => {
       <button
         type="button"
         className="block w-1/3 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900"
-        onClick={() => (window.location.href = GET_BLOCKSCAN_URL(""))}
+        onClick={() => (window.location.href = GET_BLOCKSCAN_URL(window.location.pathname.split('/')[1]))}
       >
         <span>Withdraw Funds</span>
       </button>
